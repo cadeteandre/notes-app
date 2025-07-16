769927a24bbcc55b27a698e36bc91be072c8dd80
Andre Cadete <andrecadete70@gmail.com>
2025-07-16 12:08:25 +0200

    Organizing components separately

---
+++ components/NoteItem.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const NoteItem = ({ note }: { note: any }) => {
+    return ( 
+        <View style={styles.noteItem}>
+        <Text style={styles.noteTitle}>{note.title}</Text>
+        <Text>{note.content}</Text>
+    </View>
+     );
+}
+ 
+export default NoteItem;
+
+const styles = StyleSheet.create({
+    noteItem: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#f5f5f5",
+        padding: 15,
+        borderRadius: 5,
+        marginVertical: 5,
+    },
+    noteTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+})
